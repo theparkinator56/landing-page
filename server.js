5d56c105994b2b5c62d41775a1a59918067a020c
theparkinator56 <theparkinator56@gmail.com>
2019-08-12 19:15:00 -0500

    finished layout

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Hello Bois')
+const express = require('express');
+const path = require('path');
+const router = express.Router()
+
+const app = express();
+
+router.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log('Server listening on Port 3000')
+});
