5b2887037afe5d2679873fb057f46427db164abc
theparkinator56 <42982709+theparkinator56@users.noreply.github.com>
2019-10-12 18:28:12 -0500

    updated look and direction
    added background image instead of hero img

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router()
+const express = require("express");
+const path = require("path");
+const router = express.Router();
 
 const app = express();
 
-router.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.use('/', router);
+app.use("/", router);
+app.use(express.static("public"));
 
 app.listen(3000, () => {
-    console.log('Server listening on Port 3000')
+  console.log("Server listening on Port 3000");
 });
